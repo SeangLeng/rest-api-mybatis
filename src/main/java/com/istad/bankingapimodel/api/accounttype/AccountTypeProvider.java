f532be70dc7a9309b467804aca53623d2c897b5e
SeangLeng <seangleng507@gmail.com>
2023-05-06 11:54:38 +0700

    restfull-api

---
+++ src/main/java/com/istad/bankingapimodel/api/accounttype/AccountTypeProvider.java
@@ -0,0 +1,13 @@
+package com.istad.bankingapimodel.api.accounttype;
+
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.jdbc.SQL;
+
+public class AccountTypeProvider {
+    public String buildSelectSQL(){
+        return new SQL(){{
+            SELECT("*");
+            FROM ("Account_types");
+        }}.toString();
+    }
+}
